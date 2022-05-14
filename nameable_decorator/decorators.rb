4c88183565fd02ca29787ad57a41f563cbab0f00
Ntare Guy <gntare2@gmail.com>
2022-05-14 14:24:26 +0200

    Implement suggested changes

--- nameable_decorator/decorators.rb
+++ nameable_decorator/decorators.rb
@@ -10,6 +10,6 @@ end
 class TrimmerDecorator < BaseDecorator
   def correct_name
     super()
-    @nameable.correct_name[0, 10]
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
