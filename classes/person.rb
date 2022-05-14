71a9fc7b4aabdd3ba7dedb7edcea8baa60959a8f
Ntare Guy <gntare2@gmail.com>
2022-05-14 18:55:50 +0200

    Fix linter issues

--- classes/person.rb
+++ classes/person.rb
@@ -14,16 +14,21 @@ class Person < Nameable
   # getters and setters
   attr_reader :id, :rentals
   attr_accessor :name, :age
+
   def can_use_services?
     @parent_permission || of_age?
   end
+
   def correct_name
     @name
   end
+
   def add_rental(date, book)
     Rental.new(date, self, book)
   end
+
   private
+
   def of_age?
     @age >= 18
   end
